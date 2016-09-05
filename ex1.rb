551acdfe56a24ae85586e6641bd68308edc1f7d9
ariawanzero <ariawan.zero@gmail.com>
2016-09-05 16:04:54 +0700

    A Good First Program

---
+++ ex1.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+print "I'm here "
+print "My Friend \n"
+
+# Puts mencetak string pada baris baru
+# Print mencetak string lanjut kalimat

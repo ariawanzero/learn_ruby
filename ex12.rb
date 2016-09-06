a575cec457590f4f9bbe3fd99356cf5dc0eb2042
ariawanzero <ariawan.zero@gmail.com>
2016-09-06 14:03:12 +0700

    Prompting People for Numbers

--- ex12.rb
+++ ex12.rb
@@ -10,3 +10,11 @@ number = another.to_i
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+print "Testing!!"
+value = gets.chomp.to_i
+puts "Your value #{value}"
+
+print "Testing Again!!"
+value_f = gets.chomp.to_f
+puts "Your valur float #{value_f}"
